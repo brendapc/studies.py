261dc10487e84bb03f989ce0c356654534ddbfee
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-09-08 10:47:38 -0300

    Create matrizes.py

---
+++ matrizes.py
@@ -0,0 +1,6 @@
+matriz = [ [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'digite um valor:'))
+    print(matriz)
+print(matriz)
